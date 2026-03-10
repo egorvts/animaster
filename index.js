a1824e04eeee2585bd39550e3f7964879c07ea0e
Anastasia Svalova <svanastasie@yandex.ru>
2026-03-10 18:52:58 +0500

    add 2

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ function addListeners() {
             animaster().fadeIn(block, 5000);
         });
 
+    document.getElementById('fadeOutPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('fadeOutBlock');
+            animaster().fadeOut(block, 5000);
+        });
+
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
@@ -30,6 +36,12 @@ function animaster() {
         element.style.transitionDuration =  `${duration}ms`;
         element.classList.remove('hide');
         element.classList.add('show');
+    }
+
+    function fadeOut(element, duration) {
+        element.style.transitionDuration = `${duration}ms`;
+        element.classList.remove('show');
+        element.classList.add('hide');
     }
 
     /**
@@ -56,6 +68,7 @@ function animaster() {
 
     return {
         fadeIn,
+        fadeOut,
         move,
         scale
     }
